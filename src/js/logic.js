3f5710e9235074c6ff18a951faac7064bad96a8a
martin-martin <breuss.martin@gmail.com>
2018-12-27 13:28:52 -0700

    select different tasks to create different color blocks

--- src/js/logic.js
+++ src/js/logic.js
@@ -1,36 +1,48 @@
 $(document).ready(function () {
 
     // create a grid of 10x10 blocks
-    let i;
-    for (i = 0; i < 100; i++) {
-        let block = $("<div>", {class: "empty"});
-        $(".boxbox").append(block);
+    for (let i = 0; i < 100; i++) {
+        let block = $("<div>", {class: "block"});
+        $(".block-box").append(block);
     }
-    // toggle color by adding/removing class to clicked box element
-    $(".empty").click(function (event) {
-        event.preventDefault()
-        $(this).toggleClass("full");
+
+    // initialize color and assign to object on click
+    let currentColor = "white"; // base empty color
+    // change color of clicked block according to currently selected color
+    $(".block").click(function (event) {
+        event.preventDefault();
+        let clickColor = $(this).css('background-color')
+        if (clickColor !== currentColor) {
+            $(this).css('background-color', currentColor);
+        } else {  // remove color if clicked on twice with same color selection
+            $(this).css('background-color', "white");
+        }
     });
+
     // allow users to add new tasks with new colors
     $(function () {
         $('#add').on('click', function () {
             let color = $('#hex-code').val();
-            console.log(color);
             let name = $('#task-name').val();
-            console.log(name);
             if(color !== '' && name !== '') {
                 let cont = $("<div class='color-card'></div>");
                 let colorElem = $("<div class='color-cube'></div>").css('background-color', color);
                 let textElem = $("<p class='color-desc'></p>").text(name);
                 $(cont).append(colorElem, textElem);
+                // TODO: add possibility to remove a task/color item after creating it
                 // $(elem).append("<button class='rem'>X</button>");
                 $('#tasks').append(cont);
                 $('#hex-code').val(""); // clear the input
                 $('#task-name').val(""); // clear the input
             }
         })
+    });
+
+    // TODO: click is bound to dynamically generated div, therefore new color/tasks don't function
+    // https://teamtreehouse.com/community/having-trouble-selecting-dynamically-created-elements-with-jquery
+    // fetch the clicked color value and assign it to the global currentColor variable
+    $(".color-card").click( function () {
+        let color = $(this).find("div.color-cube").css('background-color');
+        currentColor = color;
     })
-
-})
-
-// $('#add-task').
+});
